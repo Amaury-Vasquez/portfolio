6bab5882e6e4a4a35d1f48d7c6a0ce8a8c08886b
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2021-03-25 16:37:23 -0600

    content

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
 
 import { Header } from "../Header";
-import { Page } from "../../styles/elements";
+import { Page, Content } from "../../styles/elements";
 
 export const Layout = (props: { children: JSX.Element }) => {
   return (
     <Fragment>
       <Header />
-      <Page>{props.children}</Page>
+      <Page>
+        <Content>{props.children}</Content>
+      </Page>
     </Fragment>
   );
 };
