2e0600314731d222233444a23bed0e9c9a19f26f
Amaury Vasquez <vasq@pop-os.localdomain>
2021-03-27 14:08:35 -0600

    Suspense, Layout, About, Loader, Contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Fragment } from "react";
+import { Fragment, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import { About } from "./pages/About";
 import { Home } from "./pages/Home/index";
-import { Projects } from "./pages/Projects/index";
+import { Contact } from "./pages/Contact";
+import { Loader } from "./components/Loader";
 import { Layout } from "./components/Layout";
+import { Projects } from "./pages/Projects/index";
 import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
@@ -12,10 +15,14 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Layout>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/projects" component={Projects} />
-          </Switch>
+          <Suspense fallback={Loader}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/projects" component={Projects} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </Suspense>
         </Layout>
       </BrowserRouter>
     </Fragment>
