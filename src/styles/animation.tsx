2e249fa6b2f5d83f16520d8cd23ebbf0aa70803b
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-03 11:15:20 -0500

    Added typing, skeleton and color load animations

--- src/styles/animation.tsx
+++ src/styles/animation.tsx
@@ -33,3 +33,54 @@ export const fadeOut = ({ time = '1s', type = 'ease' } = {}) =>
   css`
     animation: ${time} ${fadeOutKeyframes} ${type};
   `;
+
+const typingKeyframes = keyframes`
+  from {
+    width: 0;
+  }
+`;
+
+const cursorKeyframes = keyframes`
+  from {
+    border-right: 2px solid white;
+  }
+`;
+
+export const typeWriter = ({ len = '20', time = 2 } = {}) => css`
+  width: ${len}ch;
+  animation: ${typingKeyframes} ${time + 's'} steps(${len}),
+    ${cursorKeyframes} ${time * 2 + 's'} linear;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const placeHolderKeyframes = keyframes`  
+  to {
+    background-position: 315px 0, 0 0, 0 190px, 50px 195px;
+  }
+`;
+
+export const skeleton = ({ time = '2s' } = {}) => css`
+  animation: ${placeHolderKeyframes} ${time} infinite;
+  background-repeat: no-repeat;
+  cursor: progress;
+  background-repeat: no-repeat;
+  background-size: 100% 100%, 100% 100%;
+  background-position: -315px 0, 0 0, 0px 190px, 50px 195px;
+`;
+
+const colorLoadKeyframes = keyframes`
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+export const colorLoading = css`
+  animation: ${colorLoadKeyframes} 1s linear infinite;
+`;
