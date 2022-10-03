6e22b5b51fa96f4c3e8f134d4e60cefad5af530f
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-03 11:07:20 -0500

    Added custom hook for routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,20 @@
 import { Suspense } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 import { Layout } from '../Layout';
+import { useAppRoutes } from '../hooks/useAppRoutes';
 import { FallbackScreen } from './FallbackScreen';
 import { GlobalStyles } from '../styles/GlobalStyles';
 
 const App = () => {
+  const generateRoutes = useAppRoutes();
   return (
     <HelmetProvider>
       <GlobalStyles />
       <BrowserRouter>
         <Layout>
-          <Suspense fallback={<FallbackScreen />}>
-            <Routes>
-              <Route element={<FallbackScreen />} path="/" />
-            </Routes>
-          </Suspense>
+          <Suspense fallback={<FallbackScreen />}>{generateRoutes()}</Suspense>
         </Layout>
       </BrowserRouter>
     </HelmetProvider>
