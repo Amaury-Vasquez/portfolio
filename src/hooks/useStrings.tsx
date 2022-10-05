434db722ea7864b8de586b7adf10f97f80faa75d
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-05 11:57:22 -0500

    Added chString function

--- src/hooks/useStrings.tsx
+++ src/hooks/useStrings.tsx
@@ -1,7 +1,14 @@
 export const useStrings = () => {
+  const chString = (str: string) => {
+    let count = 0;
+    for (let i = 0; i < str.length; i++)
+      count = str.charAt(i) === ' ' ? count + 1 : count;
+    return str.length - count;
+  };
+
   const toFirstUppercase = (str: string) => {
     return str.charAt(0).toLocaleUpperCase() + str.slice(1);
   };
 
-  return { toFirstUppercase };
+  return { chString, toFirstUppercase };
 };
