00357c55d140b91117e2490982a498b44bf7188b
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-05 11:56:44 -0500

    Added about and contact pages

--- src/hooks/useAppRoutes.tsx
+++ src/hooks/useAppRoutes.tsx
@@ -2,6 +2,8 @@ import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const Home = lazy(() => import('../pages/Home'));
+const About = lazy(() => import('../pages/About'));
+const Contact = lazy(() => import('../pages/Contact'));
 const Projects = lazy(() => import('../pages/Projects'));
 
 export const useAppRoutes = () => {
@@ -13,6 +15,14 @@ export const useAppRoutes = () => {
     {
       element: <Projects />,
       path: '/projects',
+    },
+    {
+      element: <About />,
+      path: '/about',
+    },
+    {
+      element: <Contact />,
+      path: '/contact',
     },
   ];
 
