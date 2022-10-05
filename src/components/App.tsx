08989ef5acb1841a28ad183466c054b667f91b50
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-10-05 11:46:42 -0500

    Moved route of layout component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
-import { Layout } from '../Layout';
+import { Layout } from './Layout';
 import { useAppRoutes } from '../hooks/useAppRoutes';
 import { FallbackScreen } from './FallbackScreen';
 import { GlobalStyles } from '../styles/GlobalStyles';
